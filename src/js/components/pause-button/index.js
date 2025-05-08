a55d9ca3cbef8dd00e35f9602178c624eadbe774
Maroun Baydoun <maroun.baydoun@gmail.com>
2025-05-08 22:50:16 +0200

    Refactor pause button

--- src/js/components/pause-button/index.js
+++ src/js/components/pause-button/index.js
@@ -1,38 +1,18 @@
-import pauseSvg from "../../icon/pause.svg?raw";
+import pauseSvg from "../../../icon/pause.svg?raw";
+import "./style.css";
 
 export const PauseButtonTagName = "za-pause-button";
 
 const template = document.createElement("template");
 
 template.innerHTML = `
-<style>
-  button {
-    background: none;
-    border: 0;
-    cursor: pointer;
-    width:35px;
-  }
-
-  button:focus-visible {
-    outline: 2px solid var(--color-outline);
-    outline-offset: 2px;
-  }
-
- svg {
-    fill: var(--color-black);
- }
-</style>
 <button>
 ${pauseSvg}
 </button>`;
 
 class PauseButton extends HTMLElement {
-  constructor() {
-    super();
-
-    this.attachShadow({ mode: "open" }).appendChild(
-      template.content.cloneNode(true),
-    );
+  connectedCallback() {
+    this.appendChild(template.content.cloneNode(true));
   }
 }
 
